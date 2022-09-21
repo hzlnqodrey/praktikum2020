c555f2ef2d87dbd5d2cc7839aa333905ed0c5c12
hazlanmuhammadqodri3@gmail.com <hazlanmuhammadqodri3@gmail.com>
2022-09-21 11:08:36 +0700

    IFELSE

--- Pertemuan/Tiga/1 - Conditional Expression in Rstudio.R
+++ Pertemuan/Tiga/1 - Conditional Expression in Rstudio.R
@@ -1,9 +1,19 @@
 # Pengkondisian (IF - ELSE - TERNARY)
 a = c(1:10)
-i = 3
+i = 2
+# 
+# if ( a[i] %% 2 == 0 ) {
+#   print("Angka Genap")
+# } else if (a[i] %% 2 != 0 ) {
+#   print("Angka Ganjil")
+# } else {
+#   print("Bukan Angka")
+# }
 
-if ( a[i] %% 2 == 0 ) {
-  print("Angka Genap")
-} else {
-  print("Angka Ganjil")
-}
+## Pernyataan IFELSE
+## ifelse(kondisi, #pernyataan true, #pernyataan false)
+ifelse(a%%2==0, "Angka Genap", "Angka Ganjil")
+
+## ANY dan ALL
+## ANY itu seperti OR
+## ALL itu seperti AND
