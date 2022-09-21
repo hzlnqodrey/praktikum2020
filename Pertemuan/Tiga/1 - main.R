22c4c26ed457a58ccb11e0bee7a0be38b65e076b
hazlanmuhammadqodri3@gmail.com <hazlanmuhammadqodri3@gmail.com>
2022-09-21 11:37:36 +0700

    sapply

--- Pertemuan/Tiga/1 - main.R
+++ Pertemuan/Tiga/1 - main.R
@@ -51,3 +51,12 @@ for ( i in 1:length(murder_rate)) {
 }
 
 rateintext
+
+m <- 25
+s_n <- vector(length = m) # create an empty vector
+for(n in 1:m){
+  s_n[n] <- compute_s_n(n)
+} 
+
+x <- 1:10
+sapply(x, sqrt)
